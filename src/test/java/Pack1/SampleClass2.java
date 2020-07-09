a32f8000cbcca90cb162d52607fc1e09df9c0f25
HEMA <HEMA@HEMA-PC>
2020-07-09 10:06:13 +0530

    First Commit

---
+++ src/test/java/Pack1/SampleClass2.java
@@ -0,0 +1,16 @@
+package Pack1;
+
+public class SampleClass2 {
+
+	public static void main(String[] args) {
+		String S1 = "Selenium";
+		
+		char c[] = S1.toCharArray();
+		
+		for (int i = c.length -1 ; i >= 0; i--) {
+			System.out.print(c[i]);
+		}
+				
+	}
+
+}
